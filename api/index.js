85720b02a0ef244fc8a278ba3a6ebd15e9c928b6
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-04-24 22:29:14 +0530

    second

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+
 
 dotenv.config()
 
@@ -19,6 +21,8 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use(express.json())
 
 app.use("/api/auth",authRoute)
+app.use("/api/users",userRoute)
+
 
 app.listen(8800, ()=>{
     console.log("backend server is connected")
