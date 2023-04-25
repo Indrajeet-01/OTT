7889732d1d7c9ac2ea544739496c29a217d1f159
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-04-25 21:48:01 +0530

    api complete

--- api/index.js
+++ api/index.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
+const movieRoute = require("./routes/movies")
+const listRoute = require("./routes/lists")
+
+
 
 
 dotenv.config()
@@ -22,6 +26,10 @@ app.use(express.json())
 
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
+app.use("/api/movies",movieRoute)
+app.use("/api/lists",listRoute)
+
+
 
 
 app.listen(8800, ()=>{
--- api/routes/users.js
+++ api/routes/users.js
@@ -31,4 +31,78 @@ router.put("/:id",verify, async (req,res) => {
     }
 }) 
 
+// DELETE
+
+router.delete("/:id",verify, async (req,res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        
+        try{
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted");
+        } catch(err){
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(403).json("You can delete only your account")
+    }
+}) 
+
+// GET USER
+
+router.get("/find/:id", async (req,res) => {
+    
+        try{
+            const user = await User.findById(req.params.id);
+            const {password, ...info} = user._doc
+            res.status(200).json(info);
+        } catch(err){
+            res.status(500).json(err)
+        }
+    }) 
+
+// GET ALL USERS
+
+router.get("/", verify, async (req, res) => {
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+      try {
+        const users = query
+          ? await User.find().sort({ _id: -1 }).limit(5)
+          : await User.find();
+        res.status(200).json(users);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You are not allowed to see all users!");
+    }
+  });
+
+  //GET USER STATS
+router.get("/stats", async (req, res) => {
+    const today = new Date();
+    const latYear = today.setFullYear(today.setFullYear() - 1);
+  
+    try {
+      const data = await User.aggregate([
+        {
+          $project: {
+            month: { $month: "$createdAt" },
+          },
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: 1 },
+          },
+        },
+      ]);
+      res.status(200).json(data)
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
+
 module.exports = router
