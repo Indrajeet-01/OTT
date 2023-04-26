1f4a5ebc80dbb85e6cfac53da6aed41381bd84b5
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-04-26 14:34:24 +0530

    f45f

--- admin/src/App.js
+++ admin/src/App.js
@@ -9,9 +9,34 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import { useState } from "react";
+import axios from "axios"
+import { useEffect } from "react";
 
 
 function App() {
+  const MONTHS =["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+
+  const [userStats, setUserStats] = useState([])
+
+  useEffect(() => {
+    const getStats =  async () =>{
+      try{
+        const res = await axios.get("/users/stats",{
+          headers: {
+            token: "Barer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NDY4ZmFjNWY5NjlmYTMwMTQzNDI1ZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MjQ4NTE3MCwiZXhwIjoxNjgyOTE3MTcwfQ.BmoHYmPlL-Pt9MS_8SR-IojuOLrVpSzUmXOGMIIHKRs"
+          },
+        })
+        setUserStats(res.data)
+
+      }catch(err){
+          console.log(err)
+      }
+    }
+    getStats()
+  },[])
+  console.log(userStats)
+
   return (
     <Router>
      
--- admin/src/dummyData.js
+++ admin/src/dummyData.js
@@ -1,47 +1,53 @@
 
 export const userData = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
+  {
+    name: "Jan",
+    uv: 4000,
+  },
+  {
+    name: "Feb",
+    uv: 3000,
+  },
+  {
+    name: "Mar",
+    uv: 5000,
+  },
+  {
+    name: "Apr",
+    uv: 4000,
+  },
+  {
+    name: "May",
+    uv: 3000,
+  },
+  {
+    name: "Jun",
+    uv: 2000,
+  },
+  {
+    name: "Jul",
+    uv: 4000,
+  },
+  {
+    name: "Agu",
+    uv: 3000,
+  },
+  {
+    name: "Sep",
+    uv: 4000,
+  },
+  {
+    name: "Oct",
+    uv: 1000,
+  },
+  {
+    name: "Nov",
+    uv: 4000,
+  },
+  {
+    name: "Dec",
+    uv: 3000,
+  },
   ];
   export const productData = [
     {
