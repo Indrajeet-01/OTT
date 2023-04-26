852cff5687e160992e8b4263d94485566d7b2989
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-04-26 15:05:32 +0530

    cs

--- admin/src/components/widgetLg/WidgetLg.jsx
+++ admin/src/components/widgetLg/WidgetLg.jsx
@@ -9,6 +9,7 @@ export default function WidgetLg() {
     <div className='widgetLg'>
       <h3 className="widgetLgtitle">Latest transactions</h3>
       <table className="widgetLgTable">
+        <tbody>
         <tr className="widgetLgTr">
           <th className="widgetLgTh">Customer</th>
           <th className="widgetLgTh">Date</th>
@@ -59,6 +60,7 @@ export default function WidgetLg() {
             <Button type="Approved"/>
           </td>
         </tr>
+        </tbody>
       </table>
     </div>
   )
--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import "./home.css"
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import Chart from '../../components/chart/Chart'
 import {userData} from "../../dummyData"
 import WidgetLg from '../../components/widgetLg/WidgetLg'
 import WidgetSm from '../../components/widgetSm/WidgetSm'
+import axios from 'axios'
 
 
 export default function Home() {
+
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+  const [userStats, setUserStats] = useState([])
+
+  useEffect(() => {
+    const getStats =  async () =>{
+      try{
+        const res = await axios.get("/users/stats",{
+          headers: {
+            token: "Barer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NDY4ZmFjNWY5NjlmYTMwMTQzNDI1ZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MjQ4NTE3MCwiZXhwIjoxNjgyOTE3MTcwfQ.BmoHYmPlL-Pt9MS_8SR-IojuOLrVpSzUmXOGMIIHKRs"
+          },
+        })
+        res.data.map((item) =>
+        setUserStats((prev) => [
+          ...prev, {name:MONTHS[item._id - 1], "New User":item.total},
+          ])
+        )
+      }catch(err){
+          console.log(err)
+      }
+    }
+    getStats()
+  },[MONTHS])
+  console.log(userStats)
+
   return (
     <div className='home'>
         <FeaturedInfo/>
